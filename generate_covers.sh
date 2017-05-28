3c1341d7cc4967d1b20ce680bff24452ad1e3efb
Roberto Focosi <rfocosi@gmail.com>
2017-05-28 15:39:38 -0300

    Removed cover's dependecy from image file

--- generate_covers.sh
+++ generate_covers.sh
@@ -2,46 +2,44 @@
 generatedFolder="generated"
 mkdir -p "$generatedFolder"
 outputFileName="CAH_cover"
+lineSize=12
+pointSize=28
 page="White"
 fill="black"
-rectFill="#ffffff"
+pageColor="#ffffff"
 row=0
 col=0
 newPage=1
-for i in {1..40}
+cardWidth=200
+cardHeight=260
+
+line1="Cards"
+line2="Against"
+line3="Humanity."
+
+for i in {1..32}
 do
-    if [ "$col" == "4" ]
-    then
+    if [ "$col" == "4" ]; then
        row=$((row+1))
        col=0
     fi
-    if [ "$row" == "5" ]
-    then
+    if [ "$row" == "4" ]; then
        page="Black"
        fill="white"
-       rectFill="#231f20"
+       pageColor="#231f20"
        row=0
        newPage=1
     fi
     outputFile="$generatedFolder/$outputFileName-$page.jpg"
-    sourceFile="$outputFile"
-    if [ "$newPage" == "1" ]
-    then
-       sourceFile="img/CAH_Blank""$page""Cards.jpg"
-       newPage=0
+    if [ "$newPage" == "1" ]; then
+      convert -size 850x1100 xc:$pageColor "$outputFile"
+      newPage=0
     fi
 
-    posRow=$((200*row))
-    posCol=$((200*col))
+    posCol=$((cardWidth*col))
+    posRow=$((cardHeight*row))
 
-    lineSize=12
-    pointSize=28
-
-    line1="Cards"
-    line2="Against"
-    line3="Humanity."
-
-    convert -weight bold -pointsize $((pointSize)) -fill $fill -draw "text $((posCol+42)),$((posRow+85)) '$line1' text $((posCol+42)),$((posRow+115)) '$line2' text $((posCol+42)),$((posRow+145)) '$line3' fill $rectFill rectangle $((posCol+38)),$((posRow-row+215)) $((posCol+182)),$((posRow-row+245))" "$sourceFile" "$outputFile"
+    convert -weight bold -pointsize $((pointSize)) -fill $fill -draw "text $((posCol+42)),$((posRow+85)) '$line1' text $((posCol+42)),$((posRow+115)) '$line2' text $((posCol+42)),$((posRow+145)) '$line3'" "$outputFile" "$outputFile"
 
     echo "page $page:$i"
     col=$((col + 1))
