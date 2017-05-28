e75aaabc2fae17d1e36fc298982fbd37247a61f9
Roberto Focosi <rfocosi@gmail.com>
2017-05-28 15:46:34 -0300

    Fixing identation

--- generate_covers.sh
+++ generate_covers.sh
@@ -17,30 +17,30 @@ line1="Cards"
 line2="Against"
 line3="Humanity."
 
-for i in {1..32}
-do
-    if [ "$col" == "4" ]; then
-       row=$((row+1))
-       col=0
-    fi
-    if [ "$row" == "4" ]; then
-       page="Black"
-       fill="white"
-       pageColor="#231f20"
-       row=0
-       newPage=1
-    fi
-    outputFile="$generatedFolder/$outputFileName-$page.jpg"
-    if [ "$newPage" == "1" ]; then
-      convert -size 850x1100 xc:$pageColor "$outputFile"
-      newPage=0
-    fi
+for i in {1..32}; do
+  if [ "$col" == "4" ]; then
+    row=$((row+1))
+    col=0
+  fi
+  if [ "$row" == "4" ]; then
+    page="Black"
+    fill="white"
+    pageColor="#231f20"
+    row=0
+    newPage=1
+  fi
+  outputFile="$generatedFolder/$outputFileName-$page.jpg"
+  if [ "$newPage" == "1" ]; then
+    convert -size 850x1100 xc:$pageColor "$outputFile"
+    newPage=0
+  fi
 
-    posCol=$((cardWidth*col))
-    posRow=$((cardHeight*row))
+  posCol=$((cardWidth*col))
+  posRow=$((cardHeight*row))
 
-    convert -weight bold -pointsize $((pointSize)) -fill $fill -draw "text $((posCol+42)),$((posRow+85)) '$line1' text $((posCol+42)),$((posRow+115)) '$line2' text $((posCol+42)),$((posRow+145)) '$line3'" "$outputFile" "$outputFile"
+  convert -weight bold -pointsize $((pointSize)) -fill $fill \
+    -draw "text $((posCol+42)),$((posRow+85)) '$line1' text $((posCol+42)),$((posRow+115)) '$line2' text $((posCol+42)),$((posRow+145)) '$line3'" "$outputFile" "$outputFile"
 
-    echo "page $page:$i"
-    col=$((col + 1))
+  echo "page $page:$i"
+  col=$((col + 1))
 done
