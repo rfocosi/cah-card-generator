5ced7dea9bc31e60f8d189370d717e0e9978d893
Roberto Focosi <rfocosi@gmail.com>
2017-05-27 21:38:03 -0300

    Working refactor

--- generate_cards.sh
+++ generate_cards.sh
@@ -27,6 +27,7 @@ col=0
 newPage=1
 lineSize=22
 pointSize=15
+rowSize=20
 
 while read -r line
 do
@@ -55,49 +56,33 @@ do
     posRow=$((200*row))
     posCol=$((200*col))
 
-    line1=${line:0:lineSize}
-    echo "0"
-    if [ "${#line1}" -eq "$lineSize" ]
-    then
-       line1=`echo ${line1} | perl -pe 's/\s[^\s]+$//g'`
-       echo $line1
-    fi
-    line2=${line:$((${#line1}+1)):lineSize}
-    echo $((${#line1}+1))
-    if [ "${#line2}" -eq "$lineSize" ]
-    then
-       line2=`echo ${line2} | perl -pe 's/\s[^\s]+$//g'`
-       echo $line2
-    fi
-    line3=${line:$((${#line1} + ${#line2} + 2)):lineSize}
-    echo $((${#line1} + ${#line2} + 2))
-    if [ "${#line3}" -eq "$lineSize" ]
-    then
-       line3=`echo ${line3} | perl -pe 's/\s[^\s]+$//g'`
-       echo $line3
-    fi
+    l=0
+    lp=0
+    more=1
+    textRows=()
+    while [ $more -eq 1 ]; do
+      more=0
+      textRows[l]=${line:${lp}:lineSize}
+      if [ "${#textRows[l]}" -eq "$lineSize" ]
+      then
+         textRows[l]=`echo ${textRows[l]} | perl -pe 's/\s[^\s]+$//g'`
+         lp=$((1+$lp+${#textRows[l]}))
+         more=1
+      fi
+      l=$((l+1))
+    done
 
-    line4=${line:$((${#line1} + ${#line2} + ${#line3} + 3)):lineSize}
-    echo $((${#line1} + ${#line2} + ${#line3} + 3))
-    if [ "${#line4}" -eq "$lineSize" ]
-    then
-       line4=`echo ${line4} | perl -pe 's/\s[^\s]+$//g'`
-       echo $line4
-    fi
+    currentLine=$((posRow+85))
+    for textRow in "${textRows[@]}"; do
+      convert -weight bold -pointsize $((pointSize)) -fill $fontColor -draw "text $((posCol+42)),$((currentLine)) '$textRow'" "$sourceFile" "$outputFile"
+      sourceFile="$outputFile"
+      currentLine=$((currentLine+rowSize))
+    done
 
-    line5=${line:$((${#line1} + ${#line2} + ${#line3} + ${#line4} + 4)):lineSize}
-    echo $((${#line1} + ${#line2} + ${#line3} + ${#line4} + 4))
-    echo $line5
-    if [ "${#line5}" -eq "$lineSize" ]
-    then
-       line5=`echo ${line5} | perl -pe 's/\s[^\s]+$//g'`
-    fi
-
-    #convert -weight bold -pointsize $((pointSize)) -fill $fontColor -draw "text $((posCol+42)),$((posRow+85)) '$line1' text $((posCol+42)),$((posRow+115)) '$line2' text $((posCol+42)),$((posRow+145)) '$line3' text $((posCol+42)),$((posRow+175)) '$line4' text $((posCol+42)),$((posRow+205)) '$line5'" "$sourceFile" "$outputFile"
     if [ "$pick" == "2" ]
     then
        convert -draw "image over $((posCol+42)),$((posRow-row+215)),0,0 'CAH_black_pick2.jpg'" "$outputFile" "$outputFile"
     fi
-    #echo "page $page:$line"
+    echo "page $page:$line"
     col=$((col + 1))
 done < "$inputFile"
