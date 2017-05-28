a0503ae7beee68e41a7ee4576878917f45982e1e
Roberto Focosi <rfocosi@gmail.com>
2017-05-28 12:33:38 -0300

    Fixing identation

--- generate_cards.sh
+++ generate_cards.sh
@@ -31,60 +31,60 @@ rowSize=20
 
 while read -r line
 do
-    text=$(echo $line | cut -d\; -f1)
-    pick=$(echo $line | cut -d\; -f2 -s)    
+  text=$(echo $line | cut -d\; -f1)
+  pick=$(echo $line | cut -d\; -f2 -s)
 
-    if [ "$col" == "4" ]; then
-       row=$((row+1))
-       col=0
+  if [ "$col" == "4" ]; then
+    row=$((row+1))
+    col=0
+  fi
+  if [ "$row" == "5" ]; then
+    page=$((page + 1))
+    row=0
+    newPage=1
+  fi
+
+  outputFile="$generatedFolder/$outputFileName-$page.jpg"
+  if [ "$newPage" == "1" ]; then
+    cp $sourceImage $outputFile
+    newPage=0
+  fi
+
+  posRow=$((200*row))
+  posCol=$((200*col))
+
+  l=0
+  lp=0
+  more=1
+  textRows=()
+  while [ $more -eq 1 ]; do
+    more=0
+    textRows[l]=${text:${lp}:lineSize}
+    if [ "${#textRows[l]}" -eq "$lineSize" ]; then
+      textRows[l]=`echo ${textRows[l]} | perl -pe 's/\s[^\s]+$//g'`
+      lp=$((1+$lp+${#textRows[l]}))
+      more=1
     fi
-    if [ "$row" == "5" ]; then
-       page=$((page + 1))
-       row=0
-       newPage=1
-    fi
+    l=$((l+1))
+  done
 
-    outputFile="$generatedFolder/$outputFileName-$page.jpg"
-    if [ "$newPage" == "1" ]; then
-      cp $sourceImage $outputFile
-      newPage=0
-    fi
+  currentCol=$((posCol+42))
+  currentRow=$((posRow+85))
+  for textRow in "${textRows[@]}"; do
+    convert -weight bold -pointsize $((pointSize)) -fill $fontColor \
+      -draw "text $currentCol,$currentRow '$textRow'" "$outputFile" "$outputFile"
+    currentRow=$((currentRow+rowSize))
+  done
 
-    posRow=$((200*row))
-    posCol=$((200*col))
-
-    l=0
-    lp=0
-    more=1
-    textRows=()
-    while [ $more -eq 1 ]; do
-      more=0
-      textRows[l]=${text:${lp}:lineSize}
-      if [ "${#textRows[l]}" -eq "$lineSize" ]; then
-         textRows[l]=`echo ${textRows[l]} | perl -pe 's/\s[^\s]+$//g'`
-         lp=$((1+$lp+${#textRows[l]}))
-         more=1
-      fi
-      l=$((l+1))
-    done
-
-    currentCol=$((posCol+42))
-    currentRow=$((posRow+85))
-    for textRow in "${textRows[@]}"; do
-      convert -weight bold -pointsize $((pointSize)) -fill $fontColor \
-        -draw "text $currentCol,$currentRow '$textRow'" "$outputFile" "$outputFile"
-      currentRow=$((currentRow+rowSize))
-    done
-
-    bottomRow=$((posRow+165-row))
-    if [ ! "$pick" == "" ]; then
-      convert -fill white -draw "circle $((currentCol+158)),$((bottomRow+60)) $((currentCol+153)),$((bottomRow+50))" \
-        -weight bold -pointSize 15 -fill white -draw "text $((currentCol+105)),$((bottomRow+66)) 'PICK'" \
-        -weight bold -pointSize 18 -fill black -draw "text $((currentCol+154)),$((bottomRow+66)) '$pick'" \
-        -weight normal -pointSize 10 -fill white -draw "text $((currentCol+33)),$((bottomRow+64)) 'CAH'" "$outputFile" "$outputFile"
-    else
-      convert -weight normal -pointSize 10 -fill $fontColor -draw "text $((currentCol+33)),$((bottomRow+64)) 'Cards Against Humanity'" "$outputFile" "$outputFile"
-    fi
-    echo "page $page:$text"
-    col=$((col + 1))
+  bottomRow=$((posRow+165-row))
+  if [ ! "$pick" == "" ]; then
+    convert -fill white -draw "circle $((currentCol+158)),$((bottomRow+60)) $((currentCol+153)),$((bottomRow+50))" \
+      -weight bold -pointSize 15 -fill white -draw "text $((currentCol+105)),$((bottomRow+66)) 'PICK'" \
+      -weight bold -pointSize 18 -fill black -draw "text $((currentCol+154)),$((bottomRow+66)) '$pick'" \
+      -weight normal -pointSize 10 -fill white -draw "text $((currentCol+33)),$((bottomRow+64)) 'CAH'" "$outputFile" "$outputFile"
+  else
+    convert -weight normal -pointSize 10 -fill $fontColor -draw "text $((currentCol+33)),$((bottomRow+64)) 'Cards Against Humanity'" "$outputFile" "$outputFile"
+  fi
+  echo "page $page:$text"
+  col=$((col + 1))
 done < "$inputFile"
